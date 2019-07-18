b7a1ddfe40d3250312c4cc03f466c10c93260e0b
Jordyn Kim <jordyn@Jordyns-MacBook-Pro.local>
2019-07-18 12:57:30 -0400

    show both psi squared methods

--- harmonicOscillatorH2.py
+++ harmonicOscillatorH2.py
@@ -37,14 +37,21 @@ H2Wfn.setX(H2Wfn.xcoords+6.2)
 
 #first equilibrate:
 v,pop,x0Equilibration,d=H2Wfn.propagate(H2Wfn.xcoords,nEquilibrationSteps,plotWalkers=False,printFlag=False)
+
+#propagate
 vref_0,pop,x0,d=H2Wfn.propagate(x0Equilibration,nSteps)
 vref_0=np.array(vref_0)
 print("Repetition: 0")
 print("Average Energy:")
 print(np.average(vref_0))
+
 #initialize figures
 fig1 = plt.figure(1)
 fig2 = plt.figure(2)
+
+#desc propagate:
+vrefdw, popdw, xdw, descendants = H2Wfn.propagate(x0, 10)
+
 
 #graph normalized histogram of x0
 
@@ -52,11 +59,11 @@ hist, bin_edges = np.histogram(x0, bins = 100)
 norm_hist = hist/x0.size
 norm_bin = (bin_edges[:-1]+bin_edges[1:])/2
 
-ax1 = fig1.add_subplot(121)
+ax1 = fig1.add_subplot(221)
 ax1.plot(norm_bin, norm_hist,'o',color='{}'.format(1.0/(2**(1))), label='Run #{}'.format(1))
 hiscombined = norm_hist
 
-#graph cumulative histogram of x0
+#graph integrated histogram of x0
 cum_hist = np.cumsum(norm_hist)
 
 bx1 = fig2.add_subplot(121)
@@ -73,12 +80,21 @@ mod = preticks % 100 == 0
 ticks = preticks[mod]
 
 #graph psi squared
-histsqrt = (hist**2)
-norm_histsqrt = histsqrt/np.sum(histsqrt)
+histsqrt, sqrtbin_edges = np.histogram(x0, bins=bin_edges, weights=descendants)
+norm_hist2 = histsqrt * norm_hist
+norm_histsqrt = norm_hist2/np.sum(norm_hist2) 
 
-ax2 = fig1.add_subplot(122)
+ax2 = fig1.add_subplot(222)
 ax2.plot(norm_bin, norm_histsqrt,'o',color='{}'.format(1.0/(2**(1))), label='Run #{}'.format(1))
 histsqrtcombined = norm_histsqrt
+
+#graph psi squared 2
+histsqrt2 = (hist**2)
+norm_histsqrt2 = histsqrt2/np.sum(histsqrt2)
+
+ax4 = fig1.add_subplot(224)
+ax4.plot(norm_bin, norm_histsqrt2,'o',color='{}'.format(1.0/(2**(1))), label='Run #{}'.format(1))
+histsqrtcombined2 = norm_histsqrt2
 
 #plot cumulative histsqrt                                                                                                     
 cum_histsqrt = np.cumsum(norm_histsqrt)
@@ -93,7 +109,10 @@ for n in range(nReps-1):
     print("Repetition: " + str(n+1))
     print("Average Energy:")
     print(np.average(vref_0))
-#plot vref_0 over time in comparison to v avarage                                                             
+
+    #descendant propagate:
+    vrefdw, popdw, xdw, descendants = H2Wfn.propagate(x0, 10)
+    #plot vref_0 over time in comparison to v avarage                                                             
     plt.figure(3)
     steps = np.array([i+1 for i in range(nSteps)])
     plt.scatter(steps, vref_0, label='V ref for run {}'.format(2+n), s=700/nSteps)
@@ -107,13 +126,22 @@ for n in range(nReps-1):
     newhiscombined = hiscombined + norm_hist
     hiscombined = newhiscombined
 
-#plot psi squared
-    histsqrt = (hist**2)
-    norm_histsqrt =histsqrt/np.sum(histsqrt)
-    ax2.plot(norm_bin, norm_histsqrt,'o',color='{}'.format(1.0/(2**(n+2))), label='Run #{}'.format(n+2))
+    #plot psi squared
+    histsqrt, sqrtbin_edges = np.histogram(x0, bins=bin_edges, weights=descendants)
+    norm_hist2 = histsqrt *norm_hist
+    norm_histsqrt = norm_hist2/np.sum(norm_hist2)
+
+    ax2.plot(norm_bin, norm_histsqrt,'o',color='{}'.format(1.0/(2**(n+2))), label='Run#{}'.format(n+2))
 
     newhistsqrtcombined = histsqrtcombined + norm_histsqrt
     histsqrtcombined = newhistsqrtcombined
+    #plot psi squared 2
+    histsqrt2 = (hist**2)
+    norm_histsqrt2 = histsqrt2/np.sum(histsqrt2)
+    ax4.plot(norm_bin, norm_histsqrt2,'o',color='{}'.format(1.0/(2**(2+n))), label='Run #{}'.format(2+n))
+
+    newhistsqrtcombined2 = histsqrtcombined2 + norm_histsqrt2
+    histsqrtcombined2 = newhistsqrtcombined2
     
     #plot cumulative hist
     cum_hist = np.cumsum(norm_hist)
@@ -141,6 +169,16 @@ ax2.grid()
 ax2.set_title('Cumulative Probability for {} Runs'.format(nReps))
 ax2.legend(loc='best')
 
+#graph psi squared 2
+avgnormedhistsqrt2 = histsqrtcombined2/nReps
+ax4.plot(norm_bin,avgnormedhistsqrt2, label='Avg Psi Squared')
+ax4.set_xlabel('x (Bohr)')
+ax4.set_ylabel('Probability')
+ax4.grid()
+ax4.set_title('Cumulative Probability for {} Runs'.format(nReps))
+ax4.legend(loc='best')
+
+
 #graphed cumulative histogram
 bx1.set_xlabel('x (Bohr)')
 bx1.set_ylabel('frequency')
@@ -166,5 +204,14 @@ plt.title('V ref over time for {} Runs'.format(nReps))
 plt.legend(loc='best')
 plt.xlim(0,nSteps)
 
+#graph two avg's into one plot
+ax3 = fig1.add_subplot(223)
+ax3.plot(norm_bin,avgnormedhist, label='Avg Psi')
+ax3.plot(norm_bin,avgnormedhistsqrt, label='Avg Psi Squared')
+ax3.set_xlabel('x (Bohr)')
+ax3.set_ylabel('frequency')
+ax3.grid()
+ax3.set_title('Cumulative Psi and Psi^2 for {} Runs'.format(nReps))
+ax3.legend(loc='best')
 
 plt.show()
