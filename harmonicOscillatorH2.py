594a369abcc8e14f87a23ffa780583f537ffe04c
Jordyn Kim <jordyn@Jordyns-MacBook-Pro.local>
2019-07-30 16:28:36 -0400

    edited average energy graphing

--- harmonicOscillatorH2.py
+++ harmonicOscillatorH2.py
@@ -7,13 +7,11 @@ import sys
 #Conversion factor of atomic units of energy to wavenumber (inverse centimeters)
 au2wn=219474.63
 
-
 #The number of walkers
 nWalkers=10000
 H2Wfn=dmc.wavefunction(nWalkers,'Morse',plotting=False)
 TheoreticalOmega0=H2Wfn.getTheoreticalOmega0()
 print('the theoretical frequency for H2 vibration is: '+str(TheoreticalOmega0)+' cm^-1')
-
 
 #Important parameters for the MC simulation
 nReps=3  #Repeat the DMC simulation 3 times
@@ -52,12 +50,11 @@ fig2 = plt.figure(2)
 #desc propagate:
 vrefdw, popdw, xdw, descendants = H2Wfn.propagate(x0, 10)
 
-
 #graph normalized histogram of x0
-
 hist, bin_edges = np.histogram(x0, bins = 100)
 norm_hist = hist/x0.size
-norm_bin = (bin_edges[:-1]+bin_edges[1:])/2
+bin_edges = bin_edges
+norm_bin = ((bin_edges[:-1]+bin_edges[1:])/2)*0.529177249
 
 ax1 = fig1.add_subplot(221)
 ax1.plot(norm_bin, norm_hist,'o',color='{}'.format(1.0/(2**(1))), label='Run #{}'.format(1))
@@ -65,7 +62,6 @@ hiscombined = norm_hist
 
 #graph integrated histogram of x0
 cum_hist = np.cumsum(norm_hist)
-
 bx1 = fig2.add_subplot(121)
 bx1.plot(norm_bin, cum_hist,'o',color='{}'.format(1.0/(2**(1))), label='Run#{}'.format(1))
 
@@ -73,6 +69,7 @@ bx1.plot(norm_bin, cum_hist,'o',color='{}'.format(1.0/(2**(1))), label='Run#{}'.
 plt.figure(3)
 steps = np.array([i+1 for i in range(nSteps)])
 plt.scatter(steps, vref_0, label='V ref for run {}'.format(1), s=700/nSteps)
+plt.axhline(y = np.average(vref_0), color='{}'.format(1.0/(2**(1))),label='V average for run {}: {}'.format(1,np.average(vref_0)))
 
 #modify ticks                                                                                                                 
 preticks = steps
@@ -101,6 +98,9 @@ cum_histsqrt = np.cumsum(norm_histsqrt)
 bx2 = fig2.add_subplot(122)
 bx2.plot(norm_bin, cum_histsqrt,'o',color='{}'.format(1.0/(2**(1))), label='Run#{}'.format(1))
 
+#initialize empty array to collect vref avgs 
+vrefavg = [np.average(vref_0)]
+
 #then simulate n times:
 for n in range(nReps-1):
     #propagate
@@ -109,6 +109,7 @@ for n in range(nReps-1):
     print("Repetition: " + str(n+1))
     print("Average Energy:")
     print(np.average(vref_0))
+    vrefavg.append(np.average(vref_0))
 
     #descendant propagate:
     vrefdw, popdw, xdw, descendants = H2Wfn.propagate(x0, 10)
@@ -116,10 +117,12 @@ for n in range(nReps-1):
     plt.figure(3)
     steps = np.array([i+1 for i in range(nSteps)])
     plt.scatter(steps, vref_0, label='V ref for run {}'.format(2+n), s=700/nSteps)
-    #plot normalized histogram of population's x value                                                                                                                                                                 
+    plt.axhline(y = np.average(vref_0), color='{}'.format(1.0/(2**(1))),label='V average for run {}: {}'.format(n+2,np.average(vref_0)))
+    #plot normalized histogram of population's x value                                                                                                    
+  
     hist, bin_edges = np.histogram(x0, bins = bin_edges)
     norm_hist = hist/x0.size
-    norm_bin = (bin_edges[:-1]+bin_edges[1:])/2
+    norm_bin = ((bin_edges[:-1]+bin_edges[1:])/2)*0.529177249
     
     ax1.plot(norm_bin, norm_hist,'o',color='{}'.format(1.0/(2**(n+2))), label='Run #{}'.format(n+2))
 
@@ -131,7 +134,7 @@ for n in range(nReps-1):
     norm_hist2 = histsqrt *norm_hist
     norm_histsqrt = norm_hist2/np.sum(norm_hist2)
 
-    ax2.plot(norm_bin, norm_histsqrt,'o',color='{}'.format(1.0/(2**(n+2))), label='Run#{}'.format(n+2))
+    ax2.plot(norm_bin, norm_histsqrt,'o',color='{}'.format(1.0/(2**(n+2))), label='Run #{}'.format(n+2))
 
     newhistsqrtcombined = histsqrtcombined + norm_histsqrt
     histsqrtcombined = newhistsqrtcombined
@@ -145,16 +148,16 @@ for n in range(nReps-1):
     
     #plot cumulative hist
     cum_hist = np.cumsum(norm_hist)
-    bx1.plot(norm_bin, cum_hist,'o',color='{}'.format(1.0/(2**(n+2))), label='Run#{}'.format(n+2))
+    bx1.plot(norm_bin, cum_hist,'o',color='{}'.format(1.0/(2**(n+2))), label='Run #{}'.format(n+2))
 
     #plot cumulative histsqrt
     cum_histsqrt = np.cumsum(norm_histsqrt)
-    bx2.plot(norm_bin, cum_histsqrt,'o',color='{}'.format(1.0/(2**(n+2))), label='Run#{}'.format(n+2))
+    bx2.plot(norm_bin, cum_histsqrt,'o',color='{}'.format(1.0/(2**(n+2))), label='Run #{}'.format(n+2))
 
 #graph averaged histogram
 avgnormedhist = hiscombined/nReps
 ax1.plot(norm_bin,avgnormedhist, label='Avg Psi')
-ax1.set_xlabel('x (Bohr)')
+ax1.set_xlabel('x (Angstrom)')
 ax1.set_ylabel('frequency')
 ax1.grid()
 ax1.set_title('Cumulative Wavefunction for {} Runs'.format(nReps))
@@ -163,7 +166,7 @@ ax1.legend(loc='best')
 #graph psi squared
 avgnormedhistsqrt = histsqrtcombined/nReps
 ax2.plot(norm_bin,avgnormedhistsqrt, label='Avg Psi Squared')
-ax2.set_xlabel('x (Bohr)')
+ax2.set_xlabel('x (Angstrom)')
 ax2.set_ylabel('Probability')
 ax2.grid()
 ax2.set_title('Cumulative Probability for {} Runs'.format(nReps))
@@ -172,22 +175,21 @@ ax2.legend(loc='best')
 #graph psi squared 2
 avgnormedhistsqrt2 = histsqrtcombined2/nReps
 ax4.plot(norm_bin,avgnormedhistsqrt2, label='Avg Psi Squared')
-ax4.set_xlabel('x (Bohr)')
+ax4.set_xlabel('x (Angstrom)')
 ax4.set_ylabel('Probability')
 ax4.grid()
 ax4.set_title('Cumulative Probability for {} Runs'.format(nReps))
 ax4.legend(loc='best')
 
-
 #graphed cumulative histogram
-bx1.set_xlabel('x (Bohr)')
+bx1.set_xlabel('x (Angstrom)')
 bx1.set_ylabel('frequency')
 bx1.grid()
 bx1.set_title('Integrate Psi for {} Runs'.format(nReps))
 bx1.legend(loc='best')
 
 #graph cumulative histsqrt
-bx2.set_xlabel('x (Bohr)')
+bx2.set_xlabel('x (Angstrom)')
 bx2.set_ylabel('Probability')
 bx2.grid()
 bx2.set_title('Integrate Psi Squared for {} Runs'.format(nReps))
@@ -195,9 +197,10 @@ bx2.legend(loc='best')
 
 
 #graph vref
+vrefavg = np.array(vrefavg)
 plt.figure(3)
 plt.xticks(ticks)
-plt.axhline(y = np.average(vref_0), color='{}'.format(1.0/(2**(1))),label='V average for run {}: {}'.format(1,np.average(vref_0)))
+plt.axhline(y = np.average(vrefavg), color='{}'.format(1.0/(2**(1))),label='V average for run All runs: {}'.format(np.average(vrefavg)))
 plt.xlabel('Step')
 plt.ylabel('V')
 plt.title('V ref over time for {} Runs'.format(nReps))
@@ -208,7 +211,7 @@ plt.xlim(0,nSteps)
 ax3 = fig1.add_subplot(223)
 ax3.plot(norm_bin,avgnormedhist, label='Avg Psi')
 ax3.plot(norm_bin,avgnormedhistsqrt, label='Avg Psi Squared')
-ax3.set_xlabel('x (Bohr)')
+ax3.set_xlabel('x (Angstrom)')
 ax3.set_ylabel('frequency')
 ax3.grid()
 ax3.set_title('Cumulative Psi and Psi^2 for {} Runs'.format(nReps))
